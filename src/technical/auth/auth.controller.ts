3b3fb7e8def6b483ed96496def19a0808bad5898
Alex Boisseau <alex.boisseau79@gmail.com>
2022-03-27 20:52:22 +0200

    Create a LocalGuard instead of use 'magic strings' in the codebase

--- src/technical/auth/auth.controller.ts
+++ src/technical/auth/auth.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller()
 export class AuthController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/signin')
   async signin(@Request() req) {
     return req;
