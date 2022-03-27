900a41c544f79ae8f7eb06bb6a61de74231035fb
Alex Boisseau <alex.boisseau79@gmail.com>
2022-03-27 17:42:01 +0200

    Implement validateUser method in AuthService

--- src/technical/auth/auth.service.ts
+++ src/technical/auth/auth.service.ts
@@ -1,6 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  // Implement signin and signup methods here
+  constructor(private readonly usersService: UsersService) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(email);
+
+    if (!user) {
+      throw new NotFoundException('No user found for this email or pseudo.');
+    }
+
+    const passwordIsValid = await compare(user.password, pass);
+    if (!passwordIsValid) {
+      throw new NotFoundException('Invalid password');
+    }
+
+    const { password, ...result } = user;
+    return result;
+  }
 }
