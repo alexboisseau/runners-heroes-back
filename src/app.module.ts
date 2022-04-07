61c828146206869a1c2b3fed2381cd37dde69122
Alex Boisseau <alex.boisseau79@gmail.com>
2022-04-07 11:29:52 +0200

    Fix bugs due to the last action (remove useless files)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthController } from './technical/auth/auth.controller';
 import { AuthModule } from './technical/auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -8,7 +6,7 @@ import { RunsModule } from './runs/runs.module';
 
 @Module({
   imports: [UsersModule, AuthModule, RunsModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService],
+  controllers: [AuthController],
+  providers: [],
 })
 export class AppModule {}
